70fbf452c070ef5497a314f642e949a5d95d6c73
Yohei Otaki <76095760+popeye-yupi@users.noreply.github.com>
2021-12-14 17:14:44 +0900

    引数のテストを追加

--- test.sh
+++ test.sh
@@ -37,7 +37,31 @@ cp ../philo .
 chmod 755 philo
 
 target="./philo"
-testtime="20"
+testtime="42"
+
+function argument_test()
+{
+    printf "\e[94m[+] [$@] : Executing your program, please wait...\e[0m\n"
+    ($target $@ > /dev/null)&
+    i=1
+    error=0
+    while [ $i -lt $testtime ];do
+        printf "\r[%d...]" $i
+        pgrep -f $target > /dev/null
+        if [ "$?" -ne 0 ];then
+            printf "\r$GREEN[+] Test Succeeded %s$RESET \n" "✓ "
+            error=1
+            break
+        fi
+        sleep 1
+        i=$(( $i + 1 ))
+    done
+    sleep 1
+    if [ $error -eq 0 ];then
+        pkill -f $target
+        printf "\r$RED[+] Test Failed$RESET\n"
+    fi
+}
 
 function success_test()
 {
@@ -56,7 +80,7 @@ function success_test()
         sleep 1
         i=$(( $i + 1 ))
     done
-    sleep 1
+    sleep 42
     if [ $error -eq 0 ];then
         pkill -f $target
         printf "\r$GREEN[+] Test Succeeded %s$RESET \n" "✓ "
@@ -87,7 +111,20 @@ function faile_test()
     fi
 }
 
+# ARGUMENT TESTS
+printf "\e[94m[+] [=====[Argument Test]=====]\e[0m\n"
+argument_test '\0'
+argument_test '4 600 100'
+argument_test '4 600 100 100 100 100'
+argument_test '0 600 100 100'
+argument_test '4 -600 100 100'
+argument_test '4 600 2147483648 100'
+argument_test '4 600 "" 100'
+argument_test '4 test 100 100'
+argument_test '4 6x0 1x0 1x0'
+
 # SUCCESS TESTS
+printf "\e[94m[+] [=====[Success Test]=====]\e[0m\n"
 success_test '2 410 200 200'
 success_test '3 610 200 200'
 success_test '4 410 200 200'
@@ -98,6 +135,8 @@ success_test '50 410 200 200'
 success_test '100 410 200 200'
 
 # FAILE TESTS
+printf "\e[94m[+] [=====[Faile Test]=====]\e[0m\n"
+faile_test '1 600 100 100'
 faile_test '2 399 200 100'
 faile_test '3 410 200 200'
 faile_test '4 310 200 100'
